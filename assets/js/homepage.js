06c3e91754f23b606a8adaf7652c0da2d57d768a
Amanda Perry <challenge641@gmail.com>
2022-06-30 10:36:36 -0500

    set up fetch and catch so users know the app is working or else where the problem exists

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -18,14 +18,29 @@ var formSubmitHandler = function(event) {
 
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos"; 
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-      });
-    });
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if(response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+            alert("Error: GitHub User Not Found");
+            }
+        })
+        .catch(function(error) {
+            // notice this '.catch()' is getting chained onto the end of the '.then()' method
+            alert("Unable to connect to GitHub");
+        });
 };
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
     for (var i = 0; i <repos.length; i++) {
